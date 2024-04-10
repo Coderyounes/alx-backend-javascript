4e6a06869b7490488ae9ddd9c96747afa67cc3d8
Coderyounes <younesanas2021@gmail.com>
2024-04-10 21:20:51 +0000

    Task 10 : advanced , basic usage of async/await

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,14 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  let data = {};
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    data = { photo, user };
+  } catch (error) {
+    data = { photo: null, user: null };
+  }
+
+  return data;
+}
