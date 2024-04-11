57d7d73355f83e0ea4acf2e4def8ebcae1e1a721
Coderyounes <younesanas2021@gmail.com>
2024-04-11 07:47:37 +0000

    Task 5:  work with a imported class

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,36 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  set amount(value) {
+    this._amount = value;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set code(value) {
+    if (value instanceof Currency) {
+      this._currency = value;
+    } else {
+      throw new TypeError('Not Correct');
+    }
+  }
+
+  get code() {
+    return this._currency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency._name} (${this._currency._code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
