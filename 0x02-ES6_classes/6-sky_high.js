7e409aaf217c65f7d817f6e53bdc93902d983bbf
Coderyounes <younesanas2021@gmail.com>
2024-04-11 09:48:36 +0000

    refactor the Code ( small refactor)

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,17 +1,17 @@
 import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
-  constructor(floors, sqft) {
+  constructor(sqft, floors) {
     super(sqft);
     this.floors = floors;
   }
 
-  set floors(value) {
-    this._floors = value;
-  }
-
   get floors() {
     return this._floors;
+  }
+
+  set floors(value) {
+    this._floors = value;
   }
 
   evacuationWarningMessage() {
