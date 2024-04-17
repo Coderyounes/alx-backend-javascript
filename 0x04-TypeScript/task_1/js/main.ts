c14f06faf0d26f545ac2542616d6e968cc964176
Coderyounes <younesanas2021@gmail.com>
2024-04-17 09:42:37 +0100

    Task 3 : Printing teachers

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -10,3 +10,12 @@ interface Teacher {
 interface Directors extends Teacher {
     numberOfReports: number
 }
+
+interface printTeacherFunction {
+    firstName: string;
+    lastName: string;
+}
+
+function printTeacher(items: printTeacherFunction): string {
+    return items.firstName[0] + "." + " " + items.lastName;
+}
