2c54a3362e9945fd10dc0bcc7cdfaca188b12845
Coderyounes <younesanas2021@gmail.com>
2024-04-17 11:10:05 +0100

    Task 4 : Writing a class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -19,3 +19,32 @@ interface printTeacherFunction {
 function printTeacher(items: printTeacherFunction): string {
     return items.firstName[0] + "." + " " + items.lastName;
 }
+
+interface StudentConstructur {
+    firstName: string
+    lastName: string
+}
+
+interface IstudentClass {
+    firstName: string
+    lastName: string
+    workOnHomework(): string;
+    displayName(): string
+}
+
+class StudentClass implements IstudentClass {
+    firstName: string
+    lastName: string
+    constructor(firstName: string, lastName: string ) {
+        this.firstName = firstName
+        this.lastName = lastName
+    }
+
+    workOnHomework(): string {
+        return 'Currently working'
+    }
+
+    displayName(): string {
+        return this.firstName
+    }
+}
