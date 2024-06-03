431bb0f94e5bab1b794d3c554b9a0229b1dce555
Coderyounes <younesanas2021@gmail.com>
2024-06-03 16:02:58 +0100

    Moving forward in Task 2 , add some logic

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -2,15 +2,34 @@
 
 const fs = require('node:fs');
 
+const objList = [];
+
 const countStudents = (path) => {
   fs.readFile(path, 'utf8', (err, data) => {
     if (err) {
       throw new Error('Cannot load the database');
     }
-    console.log(data);
-    // write Login for the script to count student number & return it
-    // log the number of student of each field & their names
-    // example : Number of students in {FIELD}: {STUDENT NUMNER}. List: {STUNDENTS_NAMES}
+    const lines = data.split('\n');
+    const headers = lines[0].split(',');
+    const students = lines.length - 1;
+    const group = [];
+
+    console.log('Number of students: ', students);
+
+    lines.slice(1).map((data) => {
+      const values = data.split(',');
+      group.push(values);
+    });
+
+    group.map((arr) => {
+      const obj = {};
+      headers.forEach((field, idx) => {
+        obj[field] = arr[idx];
+      });
+      objList.push(obj);
+    });
+    console.log(objList);
+    // Write a Logic to separate each Students & Extract their firstname & count them
   });
 };
 
