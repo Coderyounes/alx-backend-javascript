f5e66c1a9581771d773113204cd8eddcd0557ad2
Coderyounes <younesanas2021@gmail.com>
2024-06-04 12:19:36 +0100

    Task 4 : Basic HTTP server using node http module

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,4 +1,13 @@
-// Create a Small HTTP server using http module
-// variable call app 
-// port : 1245
-// return message 'Hello Holberton School!' for any endpoint as plain text
+const http = require('http');
+
+const PORT = 1245;
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+});
+
+app.listen(PORT, () => {});
+
+module.exports = app;
