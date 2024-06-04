9aa4091ec548f6c359aa5208b7dbb871a182a36c
Coderyounes <younesanas2021@gmail.com>
2024-06-04 13:08:33 +0100

    making progess in complex http server

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,17 +1,54 @@
 const http = require('http');
+const fs = require('fs');
+
+async function countStudents(path) {
+  let data;
+  try {
+    data = await fs.promises.readFile(path, 'utf8');
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+  const students = data.split('\n')
+    .map((student) => student.split(','))
+    .filter((student) => student.length === 4 && student[0] !== 'firstname')
+    .map((student) => ({
+      firstName: student[0],
+      lastName: student[1],
+      age: student[2],
+      field: student[3],
+    }));
+  const csStudents = students
+    .filter((student) => student.field === 'CS')
+    .map((student) => student.firstName);
+  const sweStudents = students
+    .filter((student) => student.field === 'SWE')
+    .map((student) => student.firstName);
+  console.log(`Number of students: ${students.length}`);
+  console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}`);
+  console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
+  return { students, csStudents, sweStudents };
+}
 
 const PORT = 1245;
 
-const app = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Message!');
+const app = http.createServer(async (req, res) => {
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello Holberton School!');
+  }
+
+  if (req.url === '/students') {
+    res.statusCode = 200;
+    const data = countStudents('database.csv');
+    // parse the data to the Good format to display in response body
+  }
 });
 
 app.listen(PORT);
 
 module.exports = app;
 
-// in path '/' shoud display a Hello Holberton School!
 // in path '/students' should display 'This is the list of our students'
 // the Script should Take a file.csv as argument
+// the script  use countStudent function
