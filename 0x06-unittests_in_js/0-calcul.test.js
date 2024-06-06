773a503c8bf966daa81aa444a5380496b61c002f
Coderyounes <younesanas2021@gmail.com>
2024-06-06 16:01:08 +0100

    Task 0 & Task 1 tests

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,39 +2,35 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
-  it('should return 4 when a is 1 and b is 3', () => {
-    assert.strictEqual(calculateNumber(1.0, 3), 4);
+  it('floating point whole numbers', () => {
+    assert.strictEqual(calculateNumber(1.0, 2.0), 3);
   });
 
-  it('should return 5 when a is 1 and b is 3.7', () => {
-    assert.strictEqual(calculateNumber(1, 3.0), 4);
+  it('rounding down b\'s floating point fractional number', () => {
+    assert.strictEqual(calculateNumber(1.0, 2.4), 3);
   });
 
-  it('should return 5 when a is 1 and b is 3.7', () => {
-    assert.strictEqual(calculateNumber(1.0, 3.7), 5);
+  it('rounding down a and b\'s floating point fractional number', () => {
+    assert.strictEqual(calculateNumber(1.4, 2.4), 3);
   });
 
-  it('should return 5 when a is 1.2 and b is 3.7', () => {
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  it('rounding down a\'s floating point fractional number', () => {
+    assert.strictEqual(calculateNumber(1.4, 2.0), 3);
   });
 
-  it('should return 6 when a is 1.5 and b is 3.7', () => {
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  it('rounding up b\'s floating point fractional numbers', () => {
+    assert.strictEqual(calculateNumber(1.0, 2.5), 4);
   });
 
-  it('should return 0 when a is 0.1 and b is -0.1', () => {
-    assert.strictEqual(calculateNumber(0.1, -0.1), 0);
+  it('rounding up a and b\'s floating point fractional numbers', () => {
+    assert.strictEqual(calculateNumber(2.6, 2.5), 6);
   });
 
-  it('should return -4 when a is -1.5 and b is -2.5', () => {
-    assert.strictEqual(calculateNumber(-1.5, -2.5), -3);
+  it('rounding up a\'s floating point fractional numbers', () => {
+    assert.strictEqual(calculateNumber(2.6, 2.0), 5);
   });
 
-  it('should return 1 when a is 0.42 and b is 0.72', () => {
-    assert.strictEqual(calculateNumber(0.42, 0.72), 1);
-  });
-
-  it('should return 5 when a is 1.66 and b is 2.66', () => {
-    assert.strictEqual(calculateNumber(1.66666, 2.66666), 5);
+  it('rounding down a and b floating point fractional numbers with trailing 9\'s', () => {
+    assert.strictEqual(calculateNumber(2.499999, 3.499999), 5);
   });
 });
