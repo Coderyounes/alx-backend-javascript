cde6cf61512b55d02ffc91d9337f51c8f94c0a7b
Coderyounes <younesanas2021@gmail.com>
2024-06-06 13:02:28 +0100

    small adjustement

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,11 +3,11 @@ const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
   it('should return 4 when a is 1 and b is 3', () => {
-    assert.strictEqual(calculateNumber(1, 3), 4);
+    assert.strictEqual(calculateNumber(1.0, 3), 4);
   });
 
   it('should return 5 when a is 1 and b is 3.7', () => {
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.0, 3.7), 5);
   });
 
   it('should return 5 when a is 1.2 and b is 3.7', () => {
