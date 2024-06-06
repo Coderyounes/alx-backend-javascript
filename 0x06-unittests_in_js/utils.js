6ea91564e2f5c0d03d96430b3b10cac0097653b9
Coderyounes <younesanas2021@gmail.com>
2024-06-06 16:51:47 +0100

    Task 3 : Code & his test

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,26 @@
+const Utils = {
+    calculateNumber = (type, a, b) => {
+        if (type === 'SUM') {
+          const num1 = Math.round(a);
+          const num2 = Math.round(b);
+          return num1 + num2;
+        }
+      
+        if (type === 'SUBTRACT') {
+          const num1 = Math.round(a);
+          const num2 = Math.round(b);
+          return num1 - num2;
+        }
+      
+        if (type === 'DIVIDE') {
+          const num1 = Math.round(a);
+          const num2 = Math.round(b);
+          if (num2 === 0) {
+            return 'Error';
+          }
+          return num1 / num2;
+        }
+      },
+}
+
+module.exports = Utils;
